fb5299ec0f5137a553878c7aeed33d8c7929ef91
Andrew*Qin <aqin1@myseneca.ca>
2022-04-12 13:35:06 -0400

    added Nearby Search

--- Backend/Routes/mapRoutes.js
+++ Backend/Routes/mapRoutes.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 var axios = require('axios');
 
 //fetches nearby clinics according to the provided coordinates
-routes.get("/api/map/:lat/:lng", (req,res,next)=>{
+routes.get("/api/map/:lat/:lng", (req,res)=>{
     var config = {
         method: 'get',
         url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.params.lat}%2C${req.params.lng}&radius=15000&type=clinic&keyword=doctor&key=AIzaSyC2wMHrM8FI1xA8z-6VG2B6X-tzasCQShk`,
