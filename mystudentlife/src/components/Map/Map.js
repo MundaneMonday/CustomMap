a832d67646cf060a04a3b1003017473489f20b1d
Andrew Qin <andrewqin1994@hotmail.com>
2022-06-08 20:27:55 -0400

    some stuff

--- mystudentlife/src/components/Map/Map.js
+++ mystudentlife/src/components/Map/Map.js
@@ -132,7 +132,7 @@ export default function GetMap(){
            Hover Over The Map and Press Enter To Zoom Toward Postal Location
     </Form>  
         
-    <MapContainer center={[Latitude,Longitude]}  zoom={defaultZoom} minZoom={defaultZoom} maxZoom={16}>
+    <MapContainer tap={true} center={[Latitude,Longitude]}  zoom={defaultZoom} minZoom={defaultZoom} maxZoom={16}>
   <TileLayer
     
     url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'
